0823162633004638fd16e48d288db0e825d1abff
Nick <nick.moskalets8@gmail.com>
2026-06-22 16:28:33 +0200

    renamed magic number variable name to fit checkstyle and modified method to return the number within the bound

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,12 +3,12 @@ package core.basesyntax;
 import java.util.Arrays;
 
 public class Application {
-    private static final int ballsAmount = 3;
+    private static final int BALLS_COUNT = 3;
 
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[ballsAmount];
+        Ball[] balls = new Ball[BALLS_COUNT];
         for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
         }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static final int MAX_NUMBER = 101;
+    private static final int MAX_NUMBER = 100;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(MAX_NUMBER));
+        ball.setNumber(random.nextInt(MAX_NUMBER+1));
         ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
