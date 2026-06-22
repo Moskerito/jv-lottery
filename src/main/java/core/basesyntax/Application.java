4342373e32f0e8bc172f56dde239e88a9db658bd
Nick <nick.moskalets8@gmail.com>
2026-06-22 16:23:42 +0200

    added variables for magic numbers and moved Random inicialising outside ot the method on ColorSupplier class

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,11 +3,13 @@ package core.basesyntax;
 import java.util.Arrays;
 
 public class Application {
+    private static final int ballsAmount = 3;
+
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
         Lottery lottery = new Lottery();
-        Ball[] balls = new Ball[3];
-        for (int i = 0; i < 3; i++) {
+        Ball[] balls = new Ball[ballsAmount];
+        for (int i = 0; i < balls.length; i++) {
             balls[i] = lottery.getRandomBall();
         }
         System.out.println(Arrays.toString(balls));
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,12 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_NUMBER = 101;
     private Random random = new Random();
     private ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         Ball ball = new Ball();
-        ball.setNumber(random.nextInt(101));
+        ball.setNumber(random.nextInt(MAX_NUMBER));
         ball.setColor(colorSupplier.getRandomColor());
         return ball;
     }
